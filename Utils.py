86ff9cb83c806a0972e63ee2dd0f9ae311063cf0
Sabina Sloman <sabina.joslo@gmail.com>
2018-01-09 10:34:30 -0500

    Closes #9

--- Utils.py
+++ Utils.py
@@ -470,6 +470,82 @@ def Parse_Rel_RC_Comments(path,stop,vote_counting, NN, write_original,
         print("Started parsing at " + time.strftime('%l:%M%p'))
         Parser(path,stop,vote_counting,NN,write_original,download_raw,clean_raw)
 
+# Helper function for select_random_comments.
+def _select_n(n, iterable):
+    if len(iterable)<n:
+        return iterable
+    return np.random.choice(iterable, size=n)
+
+# Writes the original text of n comments from each year in years to file.
+# Intended for selecting comments for ground truth ratings to provide the neural
+# net with training data.
+#
+# Parameters:
+#   path: Path to working directory.
+#   n: Number of comments to retrieve for each year.
+#   years: years to select from.
+#   min_n_comments: Combine all comments from years with less than
+#       min_n_comments comments and select from the combined set. E.g. If
+#       min_n_comments = 5000, since there are less than 5000 (relevant)
+#       comments from 2006, 2007 and 2008, a random sample of n will be drawn
+#       from the pooled set of relevant comments from 2006, 2007 and 2008.
+#       Defaults to 5000.
+#   overwrite: If the sample file for the year exists, skip.
+def select_random_comments(path, n, years=years, min_n_comments=5000,
+                           overwrite=False):
+    years=sorted(years)
+    ct_peryear, ct_cumyear=Yearly_Counts(path)
+    ct_lu=dict((y, i) for i, y in enumerate(range(2006, 2018)))
+    early_years=[ yr for yr in years if ct_peryear[ct_lu[yr]]<min_n_comments ]
+    years=[ yr for yr in years if yr not in early_years ]
+    if path[-1]!='/':
+        path+='/'
+    sample_dir=path+'random_samples/'
+    os.system('mkdir -p {}'.format(sample_dir))
+    with open(path+'original_comm', 'r') as rfh:
+        rix=0
+        early_yr_fn=str(early_years[0])
+        if len(early_years)>1:
+            early_yr_fn+='-'+'-'.join(str(yr)[2:] for yr in early_years[1:])
+        early_yr_fn+='.txt'
+        if ( not overwrite and os.path.exists(sample_dir+early_yr_fn) ):
+            print ("{} exists. Skipping. Set overwrite to True to overwrite.".format(early_yr_fn))
+            pass
+        else:
+            with open(sample_dir+early_yr_fn, 'w') as wfh:
+                for year in early_years:
+                    start=ct_cumyear[ct_lu[year-1]] if year-1 in ct_lu else 0
+                    while start>rix:
+                        rfh.readline()
+                        rix+=1
+                    comments=[]
+                    for _ in range(start, ct_cumyear[ct_lu[year]]):
+                        comments.append(rfh.readline())
+                        rix+=1
+                    if len(comments)==0:
+                        continue
+                    for comment in _select_n(n, comments):
+                        wfh.write(comment)
+
+        for year in years:
+            if ( not overwrite and
+                 os.path.exists(sample_dir+'{}.txt'.format(year)) ):
+                print ("{} exists. Skipping. Set overwrite to True to overwrite.".format('{}.txt'.format(year)))
+                continue
+            with open(sample_dir+'{}.txt'.format(year), 'w') as wfh:
+                while start>rix:
+                    rfh.readline()
+                    rix+=1
+                comments=[]
+                for _ in range(ct_cumyear[ct_lu[year-1]],
+                               ct_cumyear[ct_lu[year]]):
+                    comments.append(rfh.readline())
+                    rix+=1
+                if len(comments)==0:
+                    continue
+                for comment in _select_n(n, comments):
+                    wfh.write(comment)
+
 ### determine what percentage of the posts in each year was relevant based on content filters
 
 # NOTE: Requires total comment counts (RC_Count_Total) from http://files.pushshift.io/reddit/comments/
@@ -1160,7 +1236,7 @@ def Yearly_Counts(path):
         raise Exception('The cummulative monthly counts could not be found')
 
     # load monthly relevant comment counts
-    with open("RC_Count_List",'r') as f:
+    with open(path+"/RC_Count_List",'r') as f:
         timelist = []
         for line in f:
             if line.strip() != "":
