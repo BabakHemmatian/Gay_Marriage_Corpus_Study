368d341d428f5c1a3e796607e43d916fd31f33bb
Babak Hemmatian <babak_hemmatian@brown.edu>
2018-01-23 09:22:32 -0500

    Updated stopwords, file names, default parameters

--- lda_config.py
+++ lda_config.py
@@ -5,14 +5,15 @@ import sys
 # Set to True when processing for the NN
 NN=False
 
-# Are we using a random subset of comments, or the whole dataset?
-ENTIRE_CORPUS=False
+# Are we using a random subset of comments, or the whole dataset? The names of output files will include the value of this variable
+# NOTE: Should be set to True if NN==True
+
+ENTIRE_CORPUS= False
 
 ### determine hyperparameters ###
 
 ### Pre-processing hyperparameters
 MaxVocab = 2000000 # maximum size of the vocabulary
-FrequencyFilter = 1 # tokens with a frequency equal or less than this number will be filtered out of the corpus
 no_below = 5 # tokens that appear in less than this number of documents in corpus will be filtered out
 no_above = 0.99 # tokens that appear in more than this fraction of documents in corpus will be filtered out
 training_fraction = 0.99 # what percentage of data will be used for training. The rest of the dataset will be used as an evaluation set for calculating perplexity
@@ -20,8 +21,8 @@ training_fraction = 0.99 # what percentage of data will be used for training. Th
 ### LDA hyperparameters
 iterations = 1000 # number of times LDA posterior distributions will be sampled
 num_threads = 5 # number of threads used for parallelized processing of comments. Only matters if using _Threaded functions
-num_topics = 50 # number of topics to be generated in each LDA sampling
-sample_topics = 0.2 # percentage of topics that will be selected for reporting based on average yearly contribution
+num_topics = 100 # number of topics to be generated in each LDA sampling
+sample_topics = 0.1 # percentage of topics that will be selected for reporting based on average yearly contribution
 topn = 40 # the number of high-probability words for each topic that will be exported
 sample_comments = 100 # number of comments that will be sampled from top topics
 min_comm_length = 20 # the minimum acceptable number of words in a sampled comment. Set to None for no length filtering
@@ -48,13 +49,18 @@ for year in years:
     for month in months:
         if year==2017 and month==10: # till Sep 2017
             break
+        dates.append((year,month))
 
 ## where the output will be stored
 # NOTE: To avoid confusion between different kinds of models, record the variables most important to your iteration in the folder name
-output_path = path + "/LDA_Full_"+str(num_topics)
+output_path = path + "/LDA_"+str(ENTIRE_CORPUS)+"_"+str(num_topics)
 
 ### Preprocessing ###
 
 ### determine the set of stopwords used in preprocessing
-nltk.download('stopwords')
-stop = set(nltk.corpus.stopwords.words('english'))
+
+keepers = ["how","should","should've","could","can","need","needn","why","few","more","most","all","any","against","because","ought","must","mustn","mustn't","shouldn","shouldn't","couldn't","couldn","shan't", "needn't"]
+stop = []
+for word in set(nltk.corpus.stopwords.words('english')):
+    if word not in keepers:
+        stop.append(str(word))
