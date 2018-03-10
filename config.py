cb536548acb651eff2bd1c4dace5f79eb6df4eb7
sab.joslo <sabina.joslo@gmail.com>
2018-03-10 17:59:17 -0500

    Gensim forces a minimum_probability of 1e-8, so I will make this explicit

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ if NN:
 # minimum_probability should be 0 if one_hot_topic_contributions is False
 if not one_hot_topic_contributions:
     try:
-        assert minimum_probability==0
+        assert minimum_probability==1e-8
     except AssertionError:
         raw_input("one_hot_topic_contributions is True. Overriding config settings and setting minimum_probability to 0. Press Enter to continue.")
-        minimum_probability=0
+        minimum_probability=1e-8
