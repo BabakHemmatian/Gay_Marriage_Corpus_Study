8d2e3469a9c592311c69166289627c9cecd22dff
Babak Hemmatian <babak_hemmatian@brown.edu>
2019-01-10 13:41:44 -0500

    Add support for sampling impactful posts
    Also includes support for sampling representative comments for a pre-determined set of top topics.

--- lda_defaults.py
+++ lda_defaults.py
@@ -19,14 +19,14 @@ ENTIRE_CORPUS = True # Are we using a random subset of comments, or the whole
 OVERWRITE = False # Overwrites existing sampled comment indices. Only matters
 # if ENTIRE_CORPUS = False
 DOWNLOAD_RAW = True # If a raw data file is not available on disk, download it
-# if DOWNLOAD_RAW set to True
 CLEAN_RAW = True # After parsing, delete the raw data file from disk
 vote_counting = False # Count number of upvotes when parsing
 WRITE_ORIGINAL = True # Write original comments to file when parsing
 
 ### Pre-processing hyperparameters
 MaxVocab = 2000000 # maximum size of the vocabulary
-FrequencyFilter = 1 # tokens with a frequency equal or less than this number will be filtered out of the corpus
+FrequencyFilter = 1 # tokens with a frequency equal or less than this number
+# will be filtered out of the corpus
 no_below = 5 # tokens that appear in less than this number of documents in
 # corpus will be filtered out
 no_above = 0.99 # tokens that appear in more than this fraction of documents in
@@ -43,7 +43,7 @@ n_random_comments = 1500 # number of comments to sample from each year for
 iterations = 1000 # number of times LDA posterior distributions will be sampled
 num_threads = 5 # number of threads used for parallelized processing of comments
 # Only matters if using _Threaded functions
-num_topics = 75 # number of topics to be generated in each LDA sampling
+num_topics = 50 # number of topics to be generated in each LDA sampling
 alpha = 0.1 # determines how many high probability topics will be assigned to a
 # document in general (not to be confused with NN l2regularization constant)
 minimum_probability = 0.01 # minimum acceptable probability for an output topic
@@ -52,26 +52,33 @@ eta = 0.1 # determines how many high probability words will be assigned to a
 # topic in general
 minimum_phi_value = 0.01 # determines the lower bound on per-term topic
 # probability. Only matters if per_word_topics = True.
-calculate_perplexity = True # whether perplexity should be calculated for the
-# LDA model
+calculate_perplexity = True # whether perplexity is calculated for the model
+calculate_coherence = True # whether umass coherence is calculated for the model
 
 ### Sampling hyperparameters
-sample_topics = 0.1 # percentage of topics that will be selected for reporting
-# based on average yearly contribution
+sample_topics = None # percentage of topics that will be selected for reporting
+# based on average yearly contribution. Set to None if choosing topics based on
+# threshold instead
+top_topic_thresh = 0.03 # threshold for determining topics to report
+top_topic_set = [4,14,22,23,27,28,47,12,29,48,49,16,33] # Choose specific topics
+# to sample comments and top words from (default is topics reported in the paper)
+# set to None to use threshold or fraction instead
 topn = 80 # the number of high-probability words for each topic to be exported
 # NOTE: Many of the words will inevitably be high probability general
 # non-content and non-framing words. So topn should be set to significantly
 # higher than the number of relevant words you wish to see
-sample_comments = 100 # number of comments that will be sampled from top topics
-min_comm_length = 40 # the minimum acceptable number of words in a sampled
+sample_comments = 25 # number of comments that will be sampled from top topics
+min_comm_length = 20 # the minimum acceptable number of words in a sampled
 # comment. Set to None for no length filtering
 # Determines how topic contributions are calculated. When set to True, the
-# topic of each word is set to be simply the most probable word. When False,
+# topic of each word is set to be simply the most probable topic. When False,
 # the topic of each word is set to the entire probability distribution over
 # num_topics topics.
 one_hot_topic_contributions=True
 # The frequency with which to calculate topic contribution
 topic_cont_freq="monthly"
+num_pop = 2000 # number of the most up- or down-voted comments sampled for model
+# comparison. Set to None for no sampling. Needs prior parsing with write_original = True
 
 ### Paths
 
@@ -97,7 +104,7 @@ for year in years:
 # NOTE: To avoid confusion between different kinds of models, record the
 # variables most important to your iteration in the folder name
 # Force this import so output_path is correctly set
-from lda_config import ENTIRE_CORPUS
+# from lda_config import ENTIRE_CORPUS
 output_path = path + "/LDA_"+str(ENTIRE_CORPUS)+"_"+str(num_topics)
 
 ### Preprocessing ###
@@ -110,4 +117,5 @@ for word in set(nltk.corpus.stopwords.words('english')):
     if word not in keepers:
         stop.append(str(word))
 
-regex=re.compile("(^(?=.*gay|.*homosexual|.*homophile|.*fag|.*faggot|.*fagot|.*queer|.*homo|.*fairy|.*nance|.*pansy|.*queen|.*LGBT|.*GLBT|.*same.sex|.*lesbian|.*dike|.*dyke|.*butch|.*sodom|.*bisexual)(?=.*marry|.*marri|.*civil union).*$)|(^(?=.*marriage equality|.*equal marriage).*$)", re.I)
+### Define the regex filter used for finding relevant comments
+regex=re.compile("(^(?=.*gay|.*\\bfag|.*faggot|.*fagot|.*queer|.*\\bhomo|.*LGBT|.*GLBT|.*same.sex|.*lesbian|.*\\bdike|.*\\bdyke|.*sodom)(?=.*marry|.*civil union|.*marri).*$)|(^(?=.*marriage equality|.*equal marriage).*$)", re.I)
